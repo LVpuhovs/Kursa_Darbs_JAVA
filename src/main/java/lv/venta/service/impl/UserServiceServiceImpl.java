8f097a5c58c48dc130a87727f4b4e7112d747aec
JanisKristsPlatais <janismarcis@gmail.com>
2024-06-24 23:23:50 +0300

    -

--- src/main/java/lv/venta/service/impl/UserServiceServiceImpl.java
+++ src/main/java/lv/venta/service/impl/UserServiceServiceImpl.java
@@ -4,7 +4,6 @@ import lv.venta.model.MyUser;
 import lv.venta.repo.IUserRepo;
 import lv.venta.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
